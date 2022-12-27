5756ca0447b05474d5b31fd50ef2e89f4eab8324
kevinnuarillo <kevinnuarillo@gmail.com>
2022-12-27 08:31:30 +0700

    new page for project

--- js/main.js
+++ js/main.js
@@ -24,3 +24,10 @@ const sr = ScrollReveal({
 sr.reveal(`.hero .container-medium`)
 sr.reveal(`.experience .container-medium`, {delay:400})
 sr.reveal(`.project, .contact`, {interval:10})
+
+/*===== SHOW SCROLL TOP =====*/
+function scrollTop(){
+	const scrollTop = document.getElementById('scroll-top');
+	if(this.scrollY >= 70) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
+ }
+ window.addEventListener('scroll', scrollTop);
