0ea4c192064f4f1a29c1b8773093156c414f63e6
kevinnuarillo <kevinnuarillo@gmail.com>
2023-01-10 11:12:08 +0700

    new project

--- js/travela.js
+++ js/travela.js
@@ -1,30 +1,3 @@
-const navMenu = document.getElementById('nav-menu'),
-			navToggle = document.getElementById('nav-toggle'),
-			navClose = document.getElementById('nav-close')
-
-if(navToggle) {
-	navToggle.addEventListener('click', () => {
-		navMenu.classList.add('show-menu')
-	})
-}
-
-if(navClose) {
-	navClose.addEventListener('click', () => {
-		navMenu.classList.remove('show-menu')
-	})
-} 
-
-const sr = ScrollReveal({
-	origin: 'top',
-	distance: '60px',
-	duration: 2000,
-	delay: 400
-})
-
-sr.reveal(`.hero .container-medium`)
-sr.reveal(`.experience .container-medium`, {delay:400})
-sr.reveal(`.project, .contact`, {interval:10})
-
 /*===== SHOW SCROLL TOP =====*/
 function scrollTop(){
 	const scrollTop = document.getElementById('scroll-top');
